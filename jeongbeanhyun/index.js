3d5d10b9e18ef4c8f8a409d49d6cb6179ebcdd49
JeongBeanHyun <kelly92108@gmail.com>
2025-05-18 21:18:16 +0900

    feat: js 추가 및 수정

---
+++ jeongbeanhyun/index.js
@@ -0,0 +1,88 @@
+//조건에 맞지 않을 경우 에러메세지
+function showError(input, errorId, message){
+    input.style.border = "2px solid red";
+    const errorDiv = document.getElementById(errorId);
+    errorDiv.textContent = message;
+    errorDiv.style.color = "red";
+}
+
+//조건 성공했을 경우
+function showSuccess(input, errorId){
+    input.style.border = "2px solid green";
+    document.getElementById(errorId).textContent = "";
+}
+
+//이름 검증
+function validateName(input, errorId){
+    let nameValue = input.value.trim();
+
+    if(nameValue === ""){
+        showError(input, errorId, "이름을 입력하세요.");
+        return false;
+    }
+
+    showSuccess(input, errorId);
+    return true;
+}
+
+//이메일 검증
+function validateEmail(input, errorId){
+    let email = input.value.trim();
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailPattern.test(email)){
+        showError(input, errorId, "유효한 이메일을 입력하세요.");
+        return false;
+    }
+
+    showSuccess(input, errorId);
+    return true;
+}
+
+//비밀번호 검증
+function validatePassword(input, errorId){
+    let pw = input.value.trim();
+    const pwPattern = /^.{6,}$/;
+
+    if(!pwPattern.test(pw)){
+        showError(input, errorId, "비밀번호는 최소 6자입니다.");
+        return false;
+    }
+
+    showSuccess(input, errorId);
+    return true;
+}
+
+//비밀번호가 일치하지 않을 경우
+function validatePasswordMatch(input, confirmInput, errorId){
+    let pw1 = input.value.trim();
+    let pw2 = confirmInput.value.trim();
+
+    if(pw1 != pw2){
+        showError(confirmInput, errorId, "비밀번호가 일치하지 않습니다.");
+        return false;
+    }
+
+    showSuccess(confirmInput, errorId);
+    return true;
+}
+
+function eventSubmit(e) {
+    e.preventDefault();
+
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("pw");
+    const passwordConfirmInput = document.getElementById("pwConfirm");
+
+    const isNameValid = validateName(nameInput, "nameError");
+    const isEmailValid = validateEmail(emailInput, "emailError");
+    const isPasswordValid = validatePassword(passwordInput, "pwError");
+    const isPasswordMatchValid = validatePasswordMatch(passwordInput, passwordConfirmInput, "pwConfirmError");
+
+    if(isNameValid && isEmailValid && isPasswordValid && isPasswordMatchValid){
+        alert("회원가입 완료!");
+    }
+}
+
+document.querySelector("form").addEventListener("submit", eventSubmit);
